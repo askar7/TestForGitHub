752991e09460b0b75726ee2094596dbaf055c0ac
askar7 <31324870+askar7@users.noreply.github.com>
2017-08-29 19:52:50 -0400

    Update MyFirstTest.java
    dwdadasdasdadasd

--- src/test/MyFirstTest.java
+++ src/test/MyFirstTest.java
@@ -2,7 +2,7 @@ package test;
 
 public class MyFirstTest {
 	public static void main(String[] args) {
-		System.out.println("Hello everobody");
+		System.out.println("Hello everobody"); //-->everobody
 	}
 
 }
